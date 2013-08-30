33c93efc7b03cf6e2cd60779d4a0ed662b29895c
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-08-30 05:36:15 -0700

    square root of BigInteger

--- Sqrt BigInt.cpp
+++ Sqrt BigInt.cpp
@@ -1,11 +1,12 @@
-
-BigInteger get(long n){return BigInteger.valueOf(n);}
-BigInteger mul(BigInteger x , BigInteger y){ return x.multiply(y);}
-BigInteger sub(BigInteger x , BigInteger y){ return x.subtract(y);}
-BigInteger add(BigInteger x , BigInteger y){ return x.add(y);}
-BigInteger div(BigInteger x , BigInteger y){ return x.divide(y);}
     
-BigInteger sqrt(BigInteger n) {
+    BigInteger get(long n){return BigInteger.valueOf(n);}
+    BigInteger mul(BigInteger x , BigInteger y){ return x.multiply(y);}
+    BigInteger sub(BigInteger x , BigInteger y){ return x.subtract(y);}
+    BigInteger add(BigInteger x , BigInteger y){ return x.add(y);}
+    BigInteger div(BigInteger x , BigInteger y){ return x.divide(y);}
+    BigInteger mod(BigInteger x , BigInteger y){ return x.mod(y);}
+    
+    BigInteger sqrt(BigInteger n) {
         BigInteger a = get(1);
         BigInteger b = new BigInteger( n.shiftRight(5).add(get(8)).toString());
         while(b.compareTo(a) >= 0) {
@@ -14,4 +15,4 @@ BigInteger sqrt(BigInteger n) {
             else a = add(mid,get(1));
          }
         return sub(a,get(1));
-}
+   }
