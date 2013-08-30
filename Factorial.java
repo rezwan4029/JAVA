dffa0b3d60a2c17f4c7e43380b273e5d84b1f80f
Rezwanul Islam Maruf <rezwan.maruf.ndc09@gmail.com>
2013-08-30 05:35:31 -0700

    factorial for BigInteger

--- Factorial.java
+++ Factorial.java
@@ -1,9 +1,10 @@
-
-  BigInteger get(long n){return BigInteger.valueOf(n);}
-  BigInteger mul(BigInteger x , BigInteger y){ return x.multiply(y);}
-  BigInteger sub(BigInteger x , BigInteger y){ return x.subtract(y);}
-  BigInteger add(BigInteger x , BigInteger y){ return x.add(y);}
-  BigInteger div(BigInteger x , BigInteger y){ return x.divide(y);}
+    
+    BigInteger get(long n){return BigInteger.valueOf(n);}
+    BigInteger mul(BigInteger x , BigInteger y){ return x.multiply(y);}
+    BigInteger sub(BigInteger x , BigInteger y){ return x.subtract(y);}
+    BigInteger add(BigInteger x , BigInteger y){ return x.add(y);}
+    BigInteger div(BigInteger x , BigInteger y){ return x.divide(y);}
+    BigInteger mod(BigInteger x , BigInteger y){ return x.mod(y);}
     
   static int SZ = 1007;
   public static BigInteger[]   fact = new BigInteger[SZ];
