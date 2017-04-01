477d3ee83b674d157705dc55819fff03dd1b29d8
Rezwanul <rezwan.islam.maruf@gmail.com>
2017-04-01 20:16:12 +0600

    Remove unnecessary codes

--- StringSplit.java
+++
@@ -1,6 +0,0 @@
-public static String[] Split(String x, String tkn) {
-        StringTokenizer StrTkn = new StringTokenizer(x, tkn);
-        ArrayList<String> ArrLis = new ArrayList<String>(x.length());
-        while (StrTkn.hasMoreTokens()) ArrLis.add(StrTkn.nextToken());
-        return ArrLis.toArray(new String[0]);
-}
